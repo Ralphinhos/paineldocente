3740af0e0f7dc01f4a21a1b37c8336783bc45f76
Raphael <raphael.guimaraes@unifenas.br>
2025-07-31 10:30:48 -0300

    botõesdevisao

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -258,13 +258,23 @@ export default function Index() {
                     <TabsList className="bg-transparent p-0 gap-4">
                         <TabsTrigger
                             value="detalhado"
-                            className="px-4 py-2 rounded-md text-sm font-medium transition-all border text-slate-500 border-slate-300 hover:bg-slate-100 dark:text-slate-400 dark:border-slate-700 dark:hover:bg-slate-800 data-[state=active]:bg-cyan-600 data-[state=active]:text-white data-[state=active]:border-cyan-600"
+                            className="px-4 py-2 rounded-md text-sm font-medium transition-all 
+                                       text-slate-500 dark:text-slate-400 border border-slate-300 dark:border-slate-700 
+                                       hover:bg-slate-100 dark:hover:bg-slate-800
+                                       data-[state=active]:bg-cyan-500 dark:data-[state=active]:bg-cyan-600 
+                                       data-[state=active]:text-white dark:data-[state=active]:text-white 
+                                       data-[state=active]:border-cyan-500 dark:data-[state=active]:border-cyan-600"
                         >
                             Visão Detalhada
                         </TabsTrigger>
                         <TabsTrigger
                             value="geral"
-                            className="px-4 py-2 rounded-md text-sm font-medium transition-all border text-slate-500 border-slate-300 hover:bg-slate-100 dark:text-slate-400 dark:border-slate-700 dark:hover:bg-slate-800 data-[state=active]:bg-cyan-600 data-[state=active]:text-white data-[state=active]:border-cyan-600"
+                            className="px-4 py-2 rounded-md text-sm font-medium transition-all 
+                                       text-slate-500 dark:text-slate-400 border border-slate-300 dark:border-slate-700 
+                                       hover:bg-slate-100 dark:hover:bg-slate-800
+                                       data-[state=active]:bg-cyan-500 dark:data-[state=active]:bg-cyan-600 
+                                       data-[state=active]:text-white dark:data-[state=active]:text-white 
+                                       data-[state=active]:border-cyan-500 dark:data-[state=active]:border-cyan-600"
                         >
                             Visão Geral
                         </TabsTrigger>
